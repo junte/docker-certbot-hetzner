0a9fdca947a1c798d490c6f8cb5a6b7f1b760ad5
Stanislav Lapshin <sm.lapshin@gmail.com>
2022-03-03 15:10:56 +0300

    fix: support for 4-level domains

--- certbot-hetzner-auth.sh
+++ certbot-hetzner-auth.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-# token=$(cat /etc/hetzner-dns-token)
 token=$HETZNER_DNS_TOKEN
-search_name=$( echo $CERTBOT_DOMAIN | rev | cut -d'.' -f 1,2 | rev)
+search_name=$( echo $CERTBOT_DOMAIN | sed 's/^\*\.//' )
 
 zone_id=$(curl \
         -H "Auth-API-Token: ${token}" \
         "https://dns.hetzner.com/api/v1/zones?search_name=${search_name}" | \
         jq ".\"zones\"[] | select(.name == \"${search_name}\") | .id" 2>/dev/null | tr -d '"')
+
+echo "zone_id: ${zone_id}"
 
 curl -X "POST" "https://dns.hetzner.com/api/v1/records" \
      -H 'Content-Type: application/json' \
